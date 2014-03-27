1f2d8d74b1d9793fc50fbac848691484af2f984f
nirenjan <nirenjan@gmail.com>
2014-03-26 21:38:30 -0700

    Update compare tests to use Comparable operators
    I've left the existing assert_equal to match against the output of the
    <=> operator, but added the new >, >=, <, <=, == and != tests to verify
    that there are no issues in Comparable

--- test/test_version_compare.rb
+++ test/test_version_compare.rb
@@ -21,28 +21,42 @@ module VMLib
 
         # Test if having identical version strings is working
         assert_equal 0, version1 <=> version2
+        assert version1 == version2
 
         # Test if having different major versions shows up correctly
         version1.major = 1
         assert_equal 1, version1 <=> version2
+        assert version1 > version2
+        assert version1 >= version2
+        assert version1 != version2
 
         # Test if having different minor versions shows up correctly
         version2.major = 1
         version2.minor = 1
         assert_equal '-1'.to_i, version1 <=> version2 
+        assert version1 != version2
+        assert version1 <= version2
+        assert version1 < version2
 
         # Test if having different patch versions shows up correctly
         version1.minor = 1
         version2.patch = 10
         assert_equal '-1'.to_i, version1 <=> version2
+        assert version1 != version2
+        assert version1 <= version2
+        assert version1 < version2
 
         # Test if setting the versions to identical one shows up as equal
         version1.patch = 10
         assert_equal 0, version1 <=> version2
+        assert version1 == version2
 
         # Test using digits of different length in the patch
         version2.patch = 9
         assert_equal 1, version1 <=> version2
+        assert version1 != version2
+        assert version1 >= version2
+        assert version1 > version2
       end
 
       # Test comparing two versions with different prerelease info
@@ -53,58 +67,72 @@ module VMLib
         # Test that a prerelease sorts lower than non-prerelease version
         version1.prerelease = ''
         assert_equal 1, version1 <=> version2
+        assert version1 > version2
 
         # Test that two dev releases sort in order
         version1.prerelease = '1'
         assert_equal 1, version1 <=> version2
+        assert version1 > version2
 
         # Test that a dev and an alpha sort with dev lower
         version1.bump_release_type
         assert_equal 1, version1 <=> version2
+        assert version1 > version2
 
         # Test that a dev and a beta sort with dev lower
         version1.bump_release_type
         assert_equal 1, version1 <=> version2
+        assert version1 > version2
 
         # Test that a dev and a rc sort with dev lower
         version1.bump_release_type
         assert_equal 1, version1 <=> version2
+        assert version1 > version2
 
         # Test that an alpha and alpha are sorted in order
         version1.prerelease = 'alpha.1'
         version2.prerelease = 'alpha.1'
         assert_equal 0, version1 <=> version2
+        assert version1 == version2
 
         version1.prerelease = 'alpha.2'
         assert_equal 1, version1 <=> version2
+        assert version1 > version2
 
         # Test that an alpha and a beta sort with alpha lower
         version1.bump_release_type
         assert_equal 1, version1 <=> version2
+        assert version1 > version2
 
         # Test that an alpha and a rc sort with alpha lower
         version1.bump_release_type
         assert_equal 1, version1 <=> version2
+        assert version1 > version2
 
         # Test that a beta and beta are sorted in order
         version1.prerelease = 'beta.1'
         version2.prerelease = 'beta.1'
         assert_equal 0, version1 <=> version2
+        assert version1 == version2
 
         version1.prerelease = 'beta.2'
         assert_equal 1, version1 <=> version2
+        assert version1 > version2
 
         # Test that a beta and a rc sort with beta lower
         version1.bump_release_type
         assert_equal 1, version1 <=> version2
+        assert version1 > version2
 
         # Test that a rc and rc are sorted in order
         version1.prerelease = 'rc.1'
         version2.prerelease = 'rc.1'
         assert_equal 0, version1 <=> version2
+        assert version1 == version2
 
         version1.prerelease = 'rc.2'
         assert_equal 1, version1 <=> version2
+        assert version1 > version2
       end
 
 
@@ -116,43 +144,53 @@ module VMLib
         version1.prerelease = 'foo.bar.baz'
         version2.prerelease = 'foo.bar.baz'
         assert_equal 0, version1 <=> version2
+        assert version1 == version2
         
         # Test that two alphanumeric prerelease strings get sorted lexically
         version1.prerelease = 'foo.bas.baz'
         assert_equal 1, version1 <=> version2
+        assert version1 > version2
 
         version1.prerelease = 'foo.bars.baz'
         assert_equal 1, version1 <=> version2
+        assert version1 > version2
 
         # Test that numeric identifiers get compared numerically
         version1.prerelease = 'foo.123'
         version2.prerelease = 'foo.21'
         assert_equal 1, version1 <=> version2
+        assert version1 > version2
 
         # Test sample test cases from mojombo/semver#100
         version1.prerelease = 'a.3.b.54'
         version2.prerelease = 'a.2.b.55'
         assert_equal 1, version1 <=> version2
+        assert version1 > version2
 
         version1.prerelease = 'a.b.2'
         version2.prerelease = 'a.1.b'
         assert_equal 1, version1 <=> version2
+        assert version1 > version2
 
         version1.prerelease = 'a.99.c.1'
         version2.prerelease = 'a.1.b.2'
         assert_equal 1, version1 <=> version2
+        assert version1 > version2
 
         version1.prerelease = 'a.b'
         version2.prerelease = 'a.1.b.2'
         assert_equal 1, version1 <=> version2
+        assert version1 > version2
 
         # Test that numeric identifiers are lower in ranking than alphanumeric
         version1.prerelease = 'a.1.2.2'
         assert_equal '-1'.to_i, version1 <=> version2
+        assert version1 < version2
 
         # Test that longer identifiers sort higher than shorter ones
         version1.prerelease = 'a.1.b.2.c'
         assert_equal 1, version1 <=> version2
+        assert version1 > version2
       end
 
 
@@ -164,24 +202,30 @@ module VMLib
         version1.build = '5c08ad0'
         version2.build = '44cbb6c'
         assert_equal 0, version1 <=> version2
+        assert version1 == version2
 
         VMLib::Version.compare_build = true
         assert_equal 1, version1 <=> version2
+        assert version1 > version2
 
         # Test that having a build metadata sorts higher than none
         version2.build = ''
         assert_equal 1, version1 <=> version2
+        assert version1 > version2
         
         version2.build = '5c08ad0'
         assert_equal 0, version1 <=> version2
+        assert version1 == version2
         
         # Test that longer build meta data sorts higher than shorter
         version1.build = '5c08ad0.20130618'
         assert_equal 1, version1 <=> version2
+        assert version1 > version2
 
         # Test that numeric identifiers sort lexically
         version2.build = '5c08ad0.102467890'
         assert_equal 1, version1 <=> version2
+        assert version1 > version2
       end
 
 
